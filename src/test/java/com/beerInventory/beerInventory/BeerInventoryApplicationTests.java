ef5ff495fa16324c7b9b86927fd3ac95c3ee8bed
kp95 <penduanttal@gmail.com>
2021-12-27 14:47:39 -0500

    Initial commit

---
+++ src/test/java/com/beerInventory/beerInventory/BeerInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerInventory.beerInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
